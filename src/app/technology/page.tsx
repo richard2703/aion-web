c00959415b0bd0a40a830066a0546bf13ad7233b
jesuslim <limchuy7@gmail.com>
2024-11-12 17:51:30 -0600

    feat translation of content

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import { Cpu } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export default function page() {
+  const t = useTranslations("TechPage");
   return (
     <div className="bg-gray-100 min-h-screen">
       {/* Hero Section */}
       <section className="bg-gradient-to-r from-slate-950 from-70%  to-[#101566] text-white py-20">
         <div className="container mx-auto px-6 max-w-screen-lg">
-          <h1 className="text-4xl font-bold mb-4">Nuestras Tecnologías</h1>
-          <p className="text-xl">
-            Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.
-          </p>
+          <h1 className="text-4xl font-bold mb-4">{t("title")}</h1>
+          <p className="text-xl">{t("description")}</p>
         </div>
       </section>
 
@@ -19,33 +19,40 @@ export default function page() {
       <section className="py-20">
         <div className="container mx-auto px-6 max-w-screen-lg">
           <h2 className="text-3xl font-bold text-center mb-12">
-            Tecnologías Clave
+            {t("key_technologies")}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+            {/* TODO: Add the custom icons from identity manifest */}
             {[
               {
                 icon: <Cpu size={40} />,
-                title: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                title: t("capsules"),
+                description: t("capsules_description"),
               },
               {
                 icon: <Cpu size={40} />,
-                title: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                title: t("tablets"),
+                description: t("tablets_description"),
               },
               {
                 icon: <Cpu size={40} />,
-                title: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                title: t("powders"),
+                description: t("powders_description"),
               },
               {
                 icon: <Cpu size={40} />,
-                title: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                title: t("liquids"),
+                description: t("liquids_description"),
+              },
+              {
+                icon: <Cpu size={40} />,
+                title: t("gels"),
+                description: t("gels_description"),
+              },
+              {
+                icon: <Cpu size={40} />,
+                title: t("ointments"),
+                description: t("ointments_description"),
               },
             ].map((tech, index) => (
               <div key={index} className="text-center">
@@ -60,41 +67,34 @@ export default function page() {
         </div>
       </section>
 
-      {/* Tools and Frameworks */}
       <section className="bg-gray-200 py-20">
         <div className="container mx-auto px-6 max-w-screen-lg">
-          <h2 className="text-3xl font-bold text-center mb-12">Herramientas</h2>
+          <h2 className="text-3xl font-bold text-center mb-12">{t("tools")}</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {[
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("edge_technology"),
+                description: t("edge_technology_description"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("information_system"),
+                description: t("information_system_description"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("Traceability"),
+                description: t("Traceability_description"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("certifications"),
+                description: t("certifications_description"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("regulation"),
+                description: t("regulation_description"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("staff"),
+                description: t("staff_description"),
               },
             ].map((tool, index) => (
               <div
@@ -115,39 +115,27 @@ export default function page() {
       <section className="py-20">
         <div className="container mx-auto px-6 max-w-screen-lg">
           <h2 className="text-3xl font-bold text-center mb-12">
-            Software y Servicios Utilizados
+            {t("resources")}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {[
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("management_system"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("certifications"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("software"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("operational_capabilities"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("Operational_excellence"),
               },
               {
-                name: "Lorem ipsum",
-                description:
-                  "Suspendisse dui mi, eleifend vel mauris in, mattis sagittis lorem.",
+                name: t("distribution_network"),
               },
             ].map((service, index) => (
               <div
@@ -157,7 +145,6 @@ export default function page() {
                 <h3 className="text-2xl font-bold text-blue-600 mb-4">
                   {service.name}
                 </h3>
-                <p className="text-gray-700">{service.description}</p>
               </div>
             ))}
           </div>
