443d02740cdedfadb550ea2d17d301a362ebc112
jesuslim <limchuy7@gmail.com>
2024-11-15 16:40:04 -0600

    feat: tecnologias card con imagnes

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -147,31 +147,60 @@ export default function page() {
               {
                 name: t("management_system"),
                 description: t("management_system_description"),
+                image: "/technologies/metro-map.png",
               },
 
               {
                 name: t("software"),
                 description: t("software_description"),
+                image: "/technologies/odoo.jpeg",
               },
               {
                 name: t("operational_capabilities"),
                 description: t("operational_capabilities_description"),
+                image: "/technologies/maquina-encapsuladora.jpeg",
               },
               {
                 name: t("Operational_excellence"),
                 description: t("Operational_excellence_description"),
+                image: "/technologies/aion.png",
               },
-            ].map((service, index) => (
-              <div
-                key={index}
-                className="text-center bg-white p-8 rounded-lg shadow-md"
-              >
-                <h3 className="text-2xl font-bold text-blue-600 mb-4">
-                  {service.name}
-                </h3>
-                <p className="font-bold text-slate-700">
-                  {service.description}
-                </p>
+            ].map((resource, index) => (
+              <div className="container mx-auto my-5">
+                <div className="relative rounded-lg flex flex-col md:flex-row items-center md:shadow-xl md:h-72 mx-2">
+                  <div className="z-0 order-1 md:order-2 relative w-full md:w-2/5 h-80 md:h-full overflow-hidden rounded-lg md:rounded-none md:rounded-r-lg">
+                    <div
+                      className="absolute inset-0 w-full h-full object-fill object-center bg-blue-400 bg-opacity-30 bg-cover bg-bottom"
+                      style={{
+                        backgroundImage: `url(${resource.image})`,
+                        backgroundBlendMode: "multiply",
+                      }}
+                    ></div>
+                    <div className="md:hidden absolute inset-0 h-full p-6 pb-6 flex flex-col-reverse justify-start items-start bg-gradient-to-b from-transparent via-transparent to-gray-900">
+                      <h3 className="w-full font-bold text-2xl text-white leading-tight mb-2">
+                        {resource.name}
+                      </h3>
+                    </div>
+                    <svg
+                      className="hidden md:block absolute inset-y-0 h-full w-24 fill-current text-white -ml-12"
+                      viewBox="0 0 100 100"
+                      preserveAspectRatio="none"
+                    >
+                      <polygon points="50,0 100,0 50,100 0,100" />
+                    </svg>
+                  </div>
+
+                  <div className="order-2 md:order-1 w-full h-full md:w-3/5 flex items-center">
+                    <div className="p-8  mx-2 md:mx-0 h-full bg-white rounded-lg md:rounded-none md:rounded-l-lg shadow-xl md:shadow-none">
+                      <h3 className="hidden md:block font-bold text-2xl text-gray-800">
+                        {resource.name}
+                      </h3>
+                      <p className="text-gray-600 text-justify">
+                        {resource.description}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
