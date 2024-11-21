818ea9e0a98eef9cba25702220a2f80b87a49c0a
jesuslim <limchuy7@gmail.com>
2024-11-21 11:09:37 -0600

    refactor: remove image links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,24 +77,6 @@ export default async function RootLayout({ children }: Props) {
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
       </head>
       <body className={`${roboto.className} bg-gray-50`}>
         <NextIntlClientProvider messages={messages}>
